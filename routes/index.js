d410529704a25ed9c07b0674b23e5ba98931cea2
Edmond Wang <644568491@qq.com>
2019-03-12 18:04:12 +0800

    Load Page with one picture

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var cars = [{
+  "name": "Honda Accord",
+  "image": "/images/honda-accord-2007-black.jpg"
+}];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { cars: cars });
 });
 
 module.exports = router;
