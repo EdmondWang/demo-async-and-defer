a52a757ce6975f3bafd79a90f2775ce6d3a31ac7
Edmond Wang <644568491@qq.com>
2019-03-13 15:14:27 +0800

    add entity code

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,17 @@ var cars = [{
   "image": "/images/bmw3.png"
 }];
 
+var dummylist = [];
+for (var i = 0; i < 10000; i++) {
+  dummylist.push({
+    name: 'Auto #' + i,
+    code: '&#' + i + ';'
+  });
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { cars: cars });
+  res.render('index', { cars: cars, dummylist: dummylist });
 });
 
 module.exports = router;
